c23d576ba7d20a6b77a71f2ec594f45831342960
DESKTOP-E001JN2\hp <m>
2024-12-10 12:18:17 +0500

    Updated

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+         "primary": "#9C27B0", 
+  "secondary": "#7B1FA2"
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1rem',
+          sm: '3rem',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
